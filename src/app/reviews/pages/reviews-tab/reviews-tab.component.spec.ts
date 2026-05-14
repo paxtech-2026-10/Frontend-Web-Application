3555f83f2b2e353bca9fcd7eeff934b287e04780
yigabriel <gabrielsacc7@gmail.com>
2026-05-14 11:01:31 -0500

    feat: add reviews test

--- src/app/reviews/pages/reviews-tab/reviews-tab.component.spec.ts
+++ src/app/reviews/pages/reviews-tab/reviews-tab.component.spec.ts
@@ -1,23 +1,65 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { TranslateModule } from '@ngx-translate/core';
 
 import { ReviewsTabComponent } from './reviews-tab.component';
+import { ReviewApiService } from '../../services/review-api.service';
+import { ReviewListComponent } from '../../../dashboard/components/review-list/review-list.component';
 
-describe('ReviewsTabComponent', () => {
+describe('ReviewsTabComponent reviews integration', () => {
+
   let component: ReviewsTabComponent;
   let fixture: ComponentFixture<ReviewsTabComponent>;
+  let reviewApiSpy: jasmine.SpyObj<ReviewApiService>;
 
   beforeEach(async () => {
+
+    reviewApiSpy = jasmine.createSpyObj('ReviewApiService', ['getBySalonId']);
+
+    reviewApiSpy.getBySalonId.and.returnValue(of([
+      {
+        id: 1,
+        author: 'Gabriel',
+        rating: 5,
+        review: 'Excellent service',
+        read: false,
+        providerId: 7
+      },
+      {
+        id: 2,
+        author: 'Maria',
+        rating: 4,
+        review: 'Good attention',
+        read: true,
+        providerId: 7
+      }
+    ]));
+
     await TestBed.configureTestingModule({
-      imports: [ReviewsTabComponent]
-    })
-    .compileComponents();
+      imports: [
+        ReviewsTabComponent,
+        ReviewListComponent,
+        TranslateModule.forRoot()
+      ],
+      providers: [
+        { provide: ReviewApiService, useValue: reviewApiSpy }
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ReviewsTabComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should load reviews and calculate average rating', () => {
+
+    spyOn(localStorage, 'getItem').and.returnValue('7');
+
+    fixture.detectChanges();
+
+    expect(component.reviews.length).toBe(2);
+    expect(component.average).toBe(4.5);
+
+    expect(component.reviews[0].author).toBe('Gabriel');
+    expect(component.reviews[1].author).toBe('Maria');
   });
 });
